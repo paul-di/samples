090f563e0e77c1c70e62df67dff3feb260f40d8d
Vasilev Pavel <PAVasilyev.SBT@sberbank.ru>
2017-04-25 10:57:55 +0300

    Simple Map reduce. add test

---
+++ SimpleMapReduce/src/test/java/com/github/paul_di/samples/mr/SimpleMapperTest.java
@@ -0,0 +1,45 @@
+package com.github.paul_di.samples.mr;
+
+import org.apache.hadoop.io.Text;
+import org.apache.orc.mapred.OrcStruct;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleMapperTest {
+  private SimpleMRMain.SimpleMapper mapper = new SimpleMRMain.SimpleMapper();
+
+  @Test(expected=IllegalArgumentException.class)
+  public void csvLineNotEnoughElements() throws Exception {
+    mapper.processCsvLine(new Text("alex,1.0"));
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void csvLineTooManyElements() throws Exception {
+    mapper.processCsvLine(new Text("alex,1.0,1,bonus string"));
+  }
+
+  @Test(expected=NumberFormatException.class)
+  public void illegalBalanceFormat() throws Exception {
+    mapper.processCsvLine(new Text("alex,1+0,1"));
+  }
+
+  @Test(expected=NumberFormatException.class)
+  public void illegalIsActiveFormat() throws Exception {
+    mapper.processCsvLine(new Text("alex,0.0,1.0"));
+  }
+
+  @Test
+  public void csvEscapingSupport() throws Exception {
+    OrcStruct row = mapper.processCsvLine(new Text("\"Alic\\\"e\",0.0,1"));
+    assertEquals(row.getFieldValue("name").toString(), "Alic\"e");
+  }
+
+  @Test
+  public void validResReturn() throws Exception {
+    OrcStruct row = mapper.processCsvLine(new Text("Bob,123456789.987654321,0"));
+    assertEquals(row.getFieldValue("name").toString(), "Bob");
+    assertEquals(row.getFieldValue("balance").toString(), "123456789.987654321");
+    assertEquals(row.getFieldValue("isActive").toString(), "0");
+  }
+}
